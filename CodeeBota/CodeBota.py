ef7138f0d9ec3b9295d249c203aee133ff2d8735
RadugaPoVenam <49412198+RadugaPoVenam@users.noreply.github.com>
2021-03-16 13:30:11 +0200

    Update CodeBota.py

--- CodeeBota/CodeBota.py
+++ CodeeBota/CodeBota.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(token)
 def qwe_text_messages(message):
     if message.text == "Привет":
         bot.send_message(message.from_user.id,"Привет,я бот Фредерико, который должен научиться показывать время")
-    elif message.text == "/help":
+    elif message.text == "help":
         bot.send_message(message.from_user.id, "Спроси меня сколько времени?")
     else:
         bot.send_message(message.from_user.id, "У меня есть только часы, большего я не могу. Могу подсказать тебе время")
