c4524bc4a5b5eb30fa9ee4352190fea07b18ae92
two-dimensional-array <max.mazyr2012@gmail.com>
2021-04-28 11:54:40 +0300

    Optimized sending a request to ESP8266 by adding a check of current state before sending the request

--- lb_4/rasp_serv.py
+++ lb_4/rasp_serv.py
@@ -12,18 +12,34 @@ sock.bind((socket_ip_addres, socket_port))
 sock.listen()
 conn, addr = sock.accept()
 
+class _state:
+    def __init__(self):
+        self.state = "off"
+    def set_state(self, state):
+        self.state = state
+    def not_equal(self, state):
+        return self.state != state
+    def equal(self, state):
+        return self.state == state
+
+state_led = _state()
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, "Здравствуйте " + str(message.chat.username) + " !\nВас приветсвует телеграм бот созданый творцом two-dimensional-array\n", reply_markup=keyboard)
 
 @bot.message_handler(commands=['on'])
 def on_led(message):
-    conn.send("on\n".encode())
+    if (state_led.not_equal("on")):
+        conn.send("on\n".encode())
+        state_led.set_state("on")
     bot.send_message(message.chat.id, "Светодиод включен", reply_markup=keyboard)
 
 @bot.message_handler(commands=['off'])
 def off_led(message):
-    conn.send("off\n".encode())
+    if (state_led.not_equal("off")):
+        conn.send("off\n".encode())
+        state_led.set_state("off")
     bot.send_message(message.chat.id, "Светодиод выключен", reply_markup=keyboard)
 
 bot.polling(none_stop=True)
